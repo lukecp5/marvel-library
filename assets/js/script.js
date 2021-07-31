7c05addffe8663d1a27599fd89371160fb61f96c
Luke Poirrier <lukepoirrier5@gmail.com>
2021-07-31 11:29:16 -0400

    Dynamically Generate cards for each character - initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,33 @@
-var characters = ["Spider-Man", "Hulk", "Captain America", "Iron Man", "Thanos", "Wolverine", "Magneto", "Daredevil", "Deadpool", "Thor"];
+var characters = [
+  "Spider-Man",
+  "Hulk",
+  "Captain America",
+  "Iron Man",
+  "Thanos",
+  "Wolverine",
+  "Magneto",
+  "Daredevil",
+  "Deadpool",
+  "Thor",
+];
+for (char in characters) {
+  var container = document.querySelector("#charCardHolder");
+  var newChar = document.createElement("div")
+  newChar.classList = "col s12 m3";
+  var content = `
+      <div class="card">
+        <div class="card-image">
+          <img src="https://materializecss.com/images/sample-1.jpg">
+          <span class="card-title">${characters[char]}</span>
+        </div>
+        <div class="card-content">
+          <p>${characters[char]} Description from marvel API</p>
+        </div>
+        <div class="card-action">
+          <a href="#">Check out ${characters[char]}'s wiki!</a>
+        </div>
+      </div>
+    </div>`;
+  newChar.innerHTML = content;
+  container.appendChild(newChar);
+}

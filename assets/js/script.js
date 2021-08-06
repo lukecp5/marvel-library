a907e90e8b130930b67140379c8b637b924c2c5c
Luke Poirrier <lukepoirrier5@gmail.com>
2021-08-05 20:55:54 -0400

    Add functionality that changes character info on main page with data from Marvel API when the user selects a character

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,129 +1,60 @@
-// this is imdb API
-function imdbApI() {
-  fetch("https://imdb-api.com/en/API/Search/k_raxvufjs/thor")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      console.log(response);
-    });
-  console.log("response.json()");
-
-  console.log(response);
-}
-console.log("response.json()", response);
-
-imdbApI;
-// end of imdb API
-
 var publicKey = "c1847813d0c19807d9ed43f48afc4f36";
-var characters = [
-  "Hulk",
-  "Captain America",
-  "Iron Man",
-  "Thanos",
-  "Wolverine",
-  "Magneto",
-  "Daredevil",
-  "Deadpool",
-  "Thor",
-];
-var charIds = {
-  Wolverine: 1009718,
-  "Captain America": "1009220",
-  Thanos: 1009652,
-  "Iron Man": 1009368,
-  Hulk: 1009351,
-  Daredevil: 1009262,
-  Magneto: 1009417,
-  Thor: 1009664,
-};
 
-var ts = Date.now();
-for (char in characters) {
-  // Base URL that we insert each characters name into to get their ID and Marvel Name
-  var baseURL =
-    "https://gateway.marvel.com:443/v1/public/characters?name=" +
-    characters[char] +
-    "&apikey=" +
-    publicKey;
+// Establish all of the required elements on the page as variables to be manipulated later
+var selectEl = document.querySelector("#characterSelect");
+var summaryTitleEl = document.querySelector("#summaryTitle");
+var nameEl = document.querySelector("#charName");
+var comicCountEl = document.querySelector("#charComicCount");
+var storyCountEl = document.querySelector("#charStoryCount");
+var marvelWikiEl = document.querySelector("#charWikiUrl");
+var charPageLinkEl = document.querySelector("#characterPageUrl");
+var thumbnailEl = document.querySelector("#characterThumbnail");
 
-  // Create a fetch function to gather the character ID's
-  fetch(baseURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      // console.log(characterID)
+function setCharacterData(){
+      // Get the value of the dropdown select box and store it in characterName
+      var characterName = selectEl.value;
+      var spiderManName = ""
+      // Set the api URL based on the selected character 
+      if(characterName == "Spider-Man"){
+            nameEl.textContent = characterName;
+            summaryTitleEl.textContent = characterName;
+            spiderManName = "Spider-Man (Peter Parker)";
+            characterName = ""
+      }
+      var lowerCasedChar = characterName.toLowerCase();
+      var baseURL = "https://gateway.marvel.com:443/v1/public/characters?name=" + characterName + spiderManName + "&apikey=" + publicKey;
+      // Set header of info cards to the name of the selected character + summary
+      summaryTitleEl.textContent = characterName + " Summary";
+      // Fetch statement to get the character ID and base data
+      fetch(baseURL)
+      .then(function(response){
+            return response.json();
+      })
+      .then(function(characterData){
+            console.log(characterData);
+            var charId = characterData.data.results[0].id;
+            var comicCount = characterData.data.results[0].comics.available;
+            var storyCount = characterData.data.results[0].stories.available;
+            // Get link to official marvel wiki for selected character
+            var charWikiUrl = characterData.data.results[0].urls[1].url;
+            // Get the path to the thumbnail for selected character
+            var thumbPath = characterData.data.results[0].thumbnail.path;
+            // Get the extension of the thumbnail for selected character
+            var thumbExt = characterData.data.results[0].thumbnail.extension;
+            // Combine the path & extension of thumbnail to get it's actual URL 
+            var thumbUrl = thumbPath + "." + thumbExt;
 
-      // This is nested inside the original fetch function so that it is called on each item returned by the first fetch statement. Assigns the character name and ID to variables for use when searching for their comics
-      console.log(data.data.results[0].name + " : " + data.data.results[0].id);
-
-      // Save the path to the useful data in the "result" variable so that we don't have to type out data.data.results[0] every time we want to use the information
-      var result = data.data.results[0];
-      var name = result.name;
-      var charID = result.id;
-
-      //The API call to the comics lists. The character ID is inserted to single out the character, and return a list of that characters comics. By default the limit is 20 results
-      fetch(
-        "https://gateway.marvel.com:443/v1/public/characters/" +
-          charID +
-          "/comics?apikey=c1847813d0c19807d9ed43f48afc4f36"
-      )
-        .then(function (response) {
-          return response.json();
-        })
-        // The data in this fetch() function is returning the JSON version of the comics for the character
-        .then(function (data) {
-          console.log(data);
-          // Here I began creating the HTML elements for each comic book returned for each character
-          // var comicList = document.createElement("ul");
-          // comicList.classList("list")
-          // for(i in data.data.results){
-          //   document.createElement("h1");
-          //   document.createElement("li")
-          // }
-        });
-    });
-
-  // The cards generated for each character
-  var container = document.querySelector("#charCardHolder");
-  var newChar = document.createElement("div");
-  newChar.classList = "col s12 m3";
-  var content = `
-  <div class="card">
-  <div class="card-image">
-  <img src="https://materializecss.com/images/sample-1.jpg">
-  <span class="card-title">${characters[char]}</span>
-  </div>
-  <div class="card-content">
-  <p>${characters[char]} Description from marvel API</p>
-  </div>
-  <div class="card-action">
-  <a href="#">Check out ${characters[char]}'s wiki!</a>
-  </div>
-  </div>
-  </div>`;
-  newChar.innerHTML = content;
-  container.appendChild(newChar);
+            if (characterName != ""){
+            nameEl.textContent = characterName;
+            }
+            thumbnailEl.src = thumbUrl;
+            comicCountEl.textContent = comicCount;
+            storyCountEl.textContent = storyCount;
+            marvelWikiEl.href = charWikiUrl;
+            marvelWikiEl.textContent = characterName + " Wiki";
+            console.log(lowerCasedChar);
+            console.log(charPageLinkEl);
+            charPageLinkEl.href= "./characters/" + lowerCasedChar + ".html";
+            console.log(charId);
+      })
 }
-// Private key is not needed for API calls, use the public key
-var privateKey = "85ab682cd5afb746ba6fa8f9a085b82a62c59778";
-marvelAPIUrl2 =
-  "https://gateway.marvel.com:443/v1/public/characters?ts=" +
-  ts +
-  "&apikey=" +
-  publicKey;
-// fetch(marvelAPIUrl2)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
-var baseURL =
-  "https://gateway.marvel.com:443/v1/public/characters?name=" +
-  char +
-  "&apikey=" +
-  publicKey;
